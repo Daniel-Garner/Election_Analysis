8cdec1c189529284f6b586d6681d3a90201d05f6
Daniel <95458216+Daniel-Garner@users.noreply.github.com>
2022-01-08 19:07:34 -0700

    upate 1

--- PyPoll1.py
+++ PyPoll1.py
@@ -14,43 +14,32 @@ with open(file_to_load) as election_data:
 
      # To do: perform analysis.
      print(election_data)
-# Add our dependencies.
 import csv
 import os
-# Assign a variable to load a file from a path.
+# Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-# Assign a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-    file_reader = csv.reader(election_data)
-
-    # Read and print the header row.
-    headers = next(file_reader)
-    print(headers)
-
-    
 
     # Print the file object.
-    # print(election_data)
+     print(election_data)
 # Create a filename variable to a direct or indirect path to the file.
-#file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Using the open() function with the "w" mode we will write data to the file.
-#open(file_to_save, "w")
+open(file_to_save, "w")
 
 # Create a filename variable to a direct or indirect path to the file.
-#file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Use the open statement to open the file as a text file.
-#outfile = open(file_to_save, "w")
+outfile = open(file_to_save, "w")
 # Write some data to the file.
-#outfile.write("Counties in the Election\n---------------\nArapahoe\nDenver\nJefferson")
+outfile.write("Counties in the Election\n---------------\nArapahoe\nDenver\nJefferson")
 
 # Close the file
-#outfile.close()
+outfile.close()
 
-
+# To do: read and analyze the data here.
 
 
 #Write down the names of all the candidates.
